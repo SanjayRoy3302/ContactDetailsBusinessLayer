64b7ca3de27251139a7f1c403047b9b06c610659
sanjay <cs@DESKTOP-Q2Q54VN>
2024-11-27 23:24:51 +0530

    BusinessLayer added

---
+++ ContactDetailsBAL.cs
@@ -0,0 +1,35 @@
+﻿using ContactDataAccessLayer;
+using ContactServiceLayer;
+using ContactServiceLayer.Models;
+
+namespace ContactBusinessLayer
+{
+    public class ContactDetailsBAL : IContactDetailsService
+    {
+        ContactDetailsDAL _DAL;
+        public ContactDetailsBAL(ContactDetailsDAL DAL) { _DAL = DAL; }
+        public List<ContactDetails> GetAllContactsList()
+        {
+            return _DAL.GetAllContactsList();
+        }
+
+        public BaseResponseModel AddContact(ContactDetails contactDetails)
+        {
+            return _DAL.AddContact(contactDetails);
+        }
+
+        public BaseResponseModel UpdateContact(ContactDetails contactDetails)
+        {
+            BaseResponseModel result = new BaseResponseModel();
+            result = _DAL.UpdateContact(contactDetails);
+            return result;
+        }
+
+        public BaseResponseModel DeleteContact(int contactDetailId)
+        {
+            BaseResponseModel result = new BaseResponseModel();
+            result = _DAL.DeleteContact(contactDetailId);
+            return result;
+        }
+    }
+}
